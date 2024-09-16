82e17d97198fe3f842ea0092609dddbaaf01423b
JuanCalle1606 <jpmilojp@gmail.com>
2024-09-16 16:30:38 -0500

    Added icons for future use

---
+++ ZZZDmgCalculator/wwwroot/web.js
@@ -0,0 +1,21 @@
+// we have JQUERY
+
+function download(url) {
+    const a = document.createElement('img')
+    a.src = url
+    return a;
+}
+
+getDivIcon = (obj) => {
+    var src = $(obj).attr("src");
+    var url =  src.replace("/48?", "/100?");
+    return download(url);
+}
+
+getIcons = (selector) => {
+    var container = $(selector);
+    
+    container.find("img").each((index, element) => {
+       container.append(getDivIcon(element));
+    });
+}
