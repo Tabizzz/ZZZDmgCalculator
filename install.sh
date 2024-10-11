60cbfe1b9245cd33c58753308f0d685ea0c127d0
JuanCalle1606 <jpmilojp@gmail.com>
2024-10-11 16:15:04 -0500

    Added wasm tools

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+dnf install -y wget
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh 
+./dotnet-install.sh
+export PATH="$PATH:/vercel/.dotnet"
+dotnet workload install wasm-tools
