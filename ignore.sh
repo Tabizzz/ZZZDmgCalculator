6f2112c396ebb95700cbbc50c75a56ea0edcae82
JuanCalle1606 <jpmilojp@gmail.com>
2024-10-16 22:39:22 -0500

    Log VERCEL vars

--- ignore.sh
+++ ignore.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_PULL_REQUEST_ID: $VERCEL_GIT_PULL_REQUEST_ID"
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+echo "VERCEL_GIT_REPO_SLUG: $VERCEL_GIT_REPO_SLUG"
+echo "VERCEL_GIT_REPO_OWNER: $VERCEL_GIT_REPO_OWNER"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "staging"  ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
+fi
 
-else
-  # Don't build
+# Don't build
   echo "🛑 - Build cancelled"
   exit 0;
-fi
